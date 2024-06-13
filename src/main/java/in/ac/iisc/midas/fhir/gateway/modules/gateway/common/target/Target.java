6b9dfd20a98ec58cdde9d602605ccdc29ebd1fa0
Farmaan Elahi <farmaan@triveous.com>
2024-06-13 18:44:32 +0530

    Fixed config

--- src/main/java/in/ac/iisc/midas/fhir/gateway/modules/gateway/common/target/Target.java
+++ src/main/java/in/ac/iisc/midas/fhir/gateway/modules/gateway/common/target/Target.java
@@ -12,6 +12,31 @@ public class Target {
     private String id;
     private String baseUrl;
     private String name;
-    private Set<String> headersToForward = Collections.emptySet();
-    private Set<String> headerToReturn = Collections.emptySet();
+    private Set<String> headersToForward = Set.of(
+            "content-type",
+            "accept-encoding",
+            "last-modified",
+            "etag",
+            "prefer",
+            "fhirVersion",
+            "if-none-exist",
+            "if-match",
+            "if-none-match",
+            "if-modified-since",
+            "if-unmodified-since",
+            "if-range",
+            "x-request-id",
+            "x-correlation-id",
+            "x-forwarded-for",
+            "x-forwarded-host"
+    );
+    private Set<String> headerToReturn = Set.of(
+            "last-modified",
+            "date",
+            "expires",
+            "etag",
+            "x-progress",
+            "x-request-id",
+            "x-correlation-id"
+    );
 }
