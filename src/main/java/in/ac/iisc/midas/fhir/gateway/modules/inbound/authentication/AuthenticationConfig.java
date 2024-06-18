3cac12539b0318c6627f96b1fad0496172bd7276
Farmaan Elahi <farmaan@triveous.com>
2024-06-19 00:05:42 +0530

    Update config

--- src/main/java/in/ac/iisc/midas/fhir/gateway/modules/inbound/authentication/AuthenticationConfig.java
+++ src/main/java/in/ac/iisc/midas/fhir/gateway/modules/inbound/authentication/AuthenticationConfig.java
@@ -1,9 +1,5 @@
 package in.ac.iisc.midas.fhir.gateway.modules.inbound.authentication;
 
-import org.keycloak.OAuth2Constants;
-import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.KeycloakBuilder;
-import org.keycloak.admin.client.resource.RealmResource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -12,62 +8,38 @@ import org.springframework.security.oauth2.jwt.JwtDecoders;
 @Conditional(InboundSecurityAuthenticationCondition.class)
 public class AuthenticationConfig {
 
-	@Bean
-	AuthenticationProperties authenticationProperties() {
-		return new AuthenticationProperties();
-	}
+    @Bean
+    AuthenticationProperties authenticationProperties() {
+        return new AuthenticationProperties();
+    }
 
-	@Bean
-	AuthenticationService authenticationService(AuthenticationProtocol.Registry registry,
-															  AuthenticationProperties properties) {
-		return new AuthenticationService(registry, properties.getProceedToAuthorizationOnFailure());
-	}
+    @Bean
+    AuthenticationService authenticationService(AuthenticationProtocol.Registry registry,
+                                                AuthenticationProperties properties) {
+        return new AuthenticationService(registry, properties.getProceedToAuthorizationOnFailure());
+    }
 
-	@Bean
-	AuthenticationInterceptor authenticationInterceptor(AuthenticationService authenticationService,
-																		 AuthenticationProperties properties) {
-		return new AuthenticationInterceptor(
-			authenticationService,
-			properties.getProceedToAuthorizationOnFailure(),
-			properties.getProceedToAuthorizationOnNoAuth());
-	}
+    @Bean
+    AuthenticationInterceptor authenticationInterceptor(AuthenticationService authenticationService,
+                                                        AuthenticationProperties properties) {
+        return new AuthenticationInterceptor(
+                authenticationService,
+                properties.getProceedToAuthorizationOnFailure(),
+                properties.getProceedToAuthorizationOnNoAuth());
+    }
 
-	@Bean
-	Keycloak keycloak(AuthenticationProperties properties) {
-		var keycloakConfig = properties.getKeycloak();
-		return KeycloakBuilder.builder()
-			.serverUrl(keycloakConfig.getBaseUri())
-			.realm(keycloakConfig.getManagementRealm())
-			.clientId(keycloakConfig.getClientId())
-			.username(keycloakConfig.getUsername())
-			.password(keycloakConfig.getPassword())
-			.grantType(OAuth2Constants.PASSWORD)
-			.build();
-	}
+    @Bean
+    public JwtDecoder jwtDecoder(AuthenticationProperties properties) {
+        return JwtDecoders.fromIssuerLocation(properties.getKeycloak().userRealmUri());
+    }
 
-	@Bean
-	KeycloakConfigurer keycloakConfigurer( RealmResource resource) {
-		return new KeycloakConfigurer(resource);
-	}
+    @Bean
+    public OIDCAuthenticationProtocol oidcAuthenticationProtocol(JwtDecoder jwtDecoder) {
+        return new OIDCAuthenticationProtocol(jwtDecoder);
+    }
 
-	@Bean
-	RealmResource userRealm(Keycloak keycloak, AuthenticationProperties properties) {
-		var keycloakConfig = properties.getKeycloak();
-		return keycloak.realm(keycloakConfig.getUserRealm());
-	}
-
-	@Bean
-	public JwtDecoder jwtDecoder(AuthenticationProperties properties) {
-		return JwtDecoders.fromIssuerLocation(properties.getKeycloak().userRealmUri());
-	}
-
-	@Bean
-	public OIDCAuthenticationProtocol oidcAuthenticationProtocol(JwtDecoder jwtDecoder) {
-		return new OIDCAuthenticationProtocol(jwtDecoder);
-	}
-
-	@Bean
-	public AuthenticationProtocol.Registry registry(OIDCAuthenticationProtocol authenticationProtocol) {
-		return new AuthenticationProtocol.Registry(authenticationProtocol);
-	}
+    @Bean
+    public AuthenticationProtocol.Registry registry(OIDCAuthenticationProtocol authenticationProtocol) {
+        return new AuthenticationProtocol.Registry(authenticationProtocol);
+    }
 }
