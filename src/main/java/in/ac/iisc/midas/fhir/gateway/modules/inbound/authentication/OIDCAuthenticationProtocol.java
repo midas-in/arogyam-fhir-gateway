c0472b43103c77b69b5a3615e6a1bb35fe1e6744
Farmaan Elahi <farmaan@triveous.com>
2024-07-11 18:29:23 +0530

    Added ability to add verify token from url

--- src/main/java/in/ac/iisc/midas/fhir/gateway/modules/inbound/authentication/OIDCAuthenticationProtocol.java
+++ src/main/java/in/ac/iisc/midas/fhir/gateway/modules/inbound/authentication/OIDCAuthenticationProtocol.java
@@ -14,108 +14,117 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 @Slf4j
 @AllArgsConstructor
 public class OIDCAuthenticationProtocol implements AuthenticationProtocol {
-	private final JwtDecoder jwtDecoder;
+    private final JwtDecoder jwtDecoder;
 
-	private static final String name = "OAuth2";
+    private static final String name = "OAuth2";
 
-	private static String getToken(RequestDetails requestDetails) {
-		var authHeader = requestDetails.getHeader(HttpHeaders.AUTHORIZATION);
-		if (authHeader == null) return null;
-		var parts = authHeader.split(" ");
-		if (parts.length < 2 || !parts[0].equalsIgnoreCase("bearer")) return null;
-		return parts[1];
-	}
+    private static String getToken(RequestDetails requestDetails) {
+        var authHeader = requestDetails.getHeader(HttpHeaders.AUTHORIZATION);
+        if (isNotBlank(authHeader)) {
+            var parts = authHeader.split(" ");
+            if (parts.length < 2 || !parts[0].equalsIgnoreCase("bearer")) return null;
+            return parts[1];
+        }
 
-	@Override
-	public boolean canAuthenticate(RequestDetails requestDetails) {
-		return StringUtils.isNotBlank(getToken(requestDetails));
-	}
+        // Get the token from the query parameter
+        var authHeaderTokens = requestDetails.getParameters().get("authToken");
+        if (authHeaderTokens.length == 0 || isBlank(authHeaderTokens[0])) return null;
+        return authHeaderTokens[0];
+    }
 
-	@Override
-	public UserSessionDetail authenticate(RequestDetails requestDetails) {
-		// Assuming this won't return a null or empty token as check might have been done using canAuthenticate
-		var token = getToken(requestDetails);
-		var jwt = jwtDecoder.decode(token);
+    @Override
+    public boolean canAuthenticate(RequestDetails requestDetails) {
+        return isNotBlank(getToken(requestDetails));
+    }
 
-		if (jwt.getSubject() == null) {
-			throw new RuntimeException("Missing subject in jwt");
-		}
+    @Override
+    public UserSessionDetail authenticate(RequestDetails requestDetails) {
+        // Assuming this won't return a null or empty token as check might have been done using canAuthenticate
+        var token = getToken(requestDetails);
+        var jwt = jwtDecoder.decode(token);
 
-		return UserSessionDetail.builder()
-			.id(jwt.getSubject())
-			.jwt(jwt)
-			.fhirContext(Collections.singletonList(requestDetails.getFhirContext()))
-			.scopes(extractScopes(jwt))
-			.oidcClientId(jwt.getClaimAsString("azp"))
-			.accountDisabled(false)
-			.email(jwt.getClaimAsString("email"))
-			.emailVerified(jwt.getClaimAsBoolean("email_verified"))
-			.authorities(extractGrantedAuthorities(jwt))
-			.accountLocked(false)
-			.authorities(extractGrantedAuthorities(jwt))
-			.givenName(jwt.getClaimAsString("given_name"))
-			.familyName(jwt.getClaimAsString("family_name"))
-			.username(jwt.getClaimAsString("preferred_username"))
-			.usernameNamespace(jwt.getClaimAsString("azp"))
-			.serviceAccount(false)
-			.fhirUserUrl(String.format("%s/%s", "Practitioner", jwt.getSubject()))
-			.build();
+        if (jwt.getSubject() == null) {
+            throw new RuntimeException("Missing subject in jwt");
+        }
 
-	}
+        return UserSessionDetail.builder()
+                .id(jwt.getSubject())
+                .jwt(jwt)
+                .fhirContext(Collections.singletonList(requestDetails.getFhirContext()))
+                .scopes(extractScopes(jwt))
+                .oidcClientId(jwt.getClaimAsString("azp"))
+                .accountDisabled(false)
+                .email(jwt.getClaimAsString("email"))
+                .emailVerified(jwt.getClaimAsBoolean("email_verified"))
+                .authorities(extractGrantedAuthorities(jwt))
+                .accountLocked(false)
+                .authorities(extractGrantedAuthorities(jwt))
+                .givenName(jwt.getClaimAsString("given_name"))
+                .familyName(jwt.getClaimAsString("family_name"))
+                .username(jwt.getClaimAsString("preferred_username"))
+                .usernameNamespace(jwt.getClaimAsString("azp"))
+                .serviceAccount(false)
+                .fhirUserUrl(String.format("%s/%s", "Practitioner", jwt.getSubject()))
+                .build();
 
-	private static Set<String> extractScopes(Jwt jwt) {
-		try {
-			return Arrays.stream(jwt.getClaimAsString("scope").split(" ")).collect(Collectors.toSet());
-		} catch (Exception e) {
-			log.error("Unable to extract scope", e);
-			return Collections.emptySet();
-		}
-	}
+    }
 
-	private static Set<UserSessionDetail.GrantedAuthority> extractGrantedAuthorities(Jwt jwt) {
-		try {
-			var realmAccess = jwt.getClaimAsMap("realm_access");
-			//noinspection unchecked,MismatchedQueryAndUpdateOfCollection
-			List<String> permissions = (realmAccess != null ?
-				((List<String>) realmAccess.getOrDefault("roles", Collections.emptyList())) :
-				Collections.emptyList());
+    private static Set<String> extractScopes(Jwt jwt) {
+        try {
+            return Arrays.stream(jwt.getClaimAsString("scope").split(" ")).collect(Collectors.toSet());
+        } catch (Exception e) {
+            log.error("Unable to extract scope", e);
+            return Collections.emptySet();
+        }
+    }
 
-			if (permissions.isEmpty()) return Collections.emptySet();
+    private static Set<UserSessionDetail.GrantedAuthority> extractGrantedAuthorities(Jwt jwt) {
+        try {
+            var realmAccess = jwt.getClaimAsMap("realm_access");
+            //noinspection unchecked,MismatchedQueryAndUpdateOfCollection
+            List<String> permissions = (realmAccess != null ?
+                    ((List<String>) realmAccess.getOrDefault("roles", Collections.emptyList())) :
+                    Collections.emptyList());
 
-			var argumentMap = new HashSetValuedHashMap<String, String>();
-			//noinspection unchecked
-			var argument = ((List<String>) jwt.getClaim("permission_argument"));
-			if (argument == null) argument = Collections.emptyList();
+            if (permissions.isEmpty()) return Collections.emptySet();
 
-			argument.stream()
-				.map(entry -> entry.split("/", 2))
-				.filter(v -> v.length == 2)
-				.forEach(v -> argumentMap.put(v[0], v[1]));
+            var argumentMap = new HashSetValuedHashMap<String, String>();
+            //noinspection unchecked
+            var argument = ((List<String>) jwt.getClaim("permission_argument"));
+            if (argument == null) argument = Collections.emptyList();
 
-			// Only accept permission as granted authorities and not the role itself
-			return permissions.stream()
-				.map(p -> {
-					try {
-						return Permission.valueOf(p);
-					} catch (IllegalArgumentException e) {
-						return null;
-					}
-				})
-				.filter(Objects::nonNull)
-				.map(p -> new UserSessionDetail.GrantedAuthority(p, argumentMap))
-				.collect(Collectors.toSet());
+            argument.stream()
+                    .map(entry -> entry.split("/", 2))
+                    .filter(v -> v.length == 2)
+                    .forEach(v -> argumentMap.put(v[0], v[1]));
 
-		} catch (Exception e) {
-			log.error("Unable to extract permission", e);
-			return Collections.emptySet();
-		}
-	}
+            // Only accept permission as granted authorities and not the role itself
+            return permissions.stream()
+                    .map(p -> {
+                        try {
+                            return Permission.valueOf(p);
+                        } catch (IllegalArgumentException e) {
+                            return null;
+                        }
+                    })
+                    .filter(Objects::nonNull)
+                    .map(p -> new UserSessionDetail.GrantedAuthority(p, argumentMap))
+                    .collect(Collectors.toSet());
 
-	@Override
-	public String name() {
-		return name;
-	}
+        } catch (Exception e) {
+            log.error("Unable to extract permission", e);
+            return Collections.emptySet();
+        }
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
 }
